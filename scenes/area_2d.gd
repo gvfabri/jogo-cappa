7f86fe1fa6ba172b719eb1df76d09068fb28a395
Gabriel Victor <143404852+gvfabri@users.noreply.github.com>
2024-10-18 17:09:15 -0300

    fase2

--- scenes/area_2d.gd
+++ scenes/area_2d.gd
@@ -1,12 +1,15 @@
 extends Area2D
 
-# Defina a nova posição para a qual o player deve ser movido
-var new_position = Vector2(1250, 800)  # Substitua por sua posição desejada
-
 func _ready():
-	# Conecta o sinal `body_entered` ao método `_on_TeleportArea_body_entered`
-	self.connect("body_entered", Callable(self, "_on_TeleportArea_body_entered"))
-	
-func _on_TeleportArea_body_entered(body):
+	# Conecta o sinal "body_entered" ao método "_on_Area2D_body_entered"
+	self.connect("body_entered", Callable(self, "_on_Area2D_body_entered"))
+
+# Função chamada quando um corpo entra na área
+func _on_Area2D_body_entered(body):
 	if body.name == "PlayerHandler":
-		body.position = new_position
+		change_level()
+
+# Função para mudar de nível
+func change_level():
+	# Carrega a nova cena do próximo nível
+	get_tree().change_scene("res://phase_2.tscn")
